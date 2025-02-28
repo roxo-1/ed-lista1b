6bea4928a7723b149021fe532c69f881be2383e0
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2025-02-28 11:46:16 +0000

    git commit

--- Main.java
+++ Main.java
@@ -1,3 +1,9 @@
-public class Main {
+//Carolina Lee - 10440304
 
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Digite alguma coisa:");
+        Palindromo p = new Palindromo("arara");
+        System.out.println(p.verificar());
+    }
 }
--- Palindromo.java
+++ Palindromo.java
@@ -1,3 +1,53 @@
-public class Palindromo {
+//Carolina Lee - 
+class Palindromo{
+    private String texto;
 
+    public Palindromo(){
+    texto="";
+    }
+
+    public Palindromo (String texto) {
+        this.setTexto(texto); 
+    }
+
+    public String getTexto(){
+        return texto;
+    }
+
+    public void setTexto(String texto){
+        if(texto == null){
+            this.texto = "";
+        } else {
+            this.texto = texto;
+        }
+    }
+
+    // public boolean verificar(){
+    //     if(texto.length() <= 1) return true;
+
+    //     String aux = texto.toLowerCase();
+    //     aux = aux.replaceAll(" ","");
+
+    //     for(int i=0; i<aux.length()/2; i++){
+    //         int pos1 = i;
+    //         int pos2 = (aux.length()-1) -i;
+    //         if (aux.charAt(pos1) != aux.charAt(pos2)) return false;
+    //     }
+    // }
+
+    public boolean verificar() {
+        if (texto == null || texto.length() <= 1) return true;
+    
+        String aux = texto.toLowerCase().replaceAll(" ", ""); // Removendo espaços e padronizando
+        int length = aux.length();
+    
+        for (int i = 0; i < length / 2; i++) {
+            if (aux.charAt(i) != aux.charAt(length - 1 - i)) {
+                return false;
+            }
+        }
+        
+        return true; // Se passou pelo loop, é palíndromo
+    }
+    
 }
