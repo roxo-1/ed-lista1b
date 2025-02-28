2204691982232ad0730c92e821a8e31093a28c3b
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2025-02-28 11:51:51 +0000

    entrada

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 //Carolina Lee - 10440304
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Digite alguma coisa:");
-        Palindromo p = new Palindromo("arara");
+        String entrada = scanner.nextLine();
+        Palindromo p = new Palindromo(entrada);
         System.out.println(p.verificar());
+        scanner.close();
     }
 }
